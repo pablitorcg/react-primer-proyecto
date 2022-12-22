7c25e8424bb48f99a168c8eb90bd323d384a2247
pablito <pirusta8@gmail.com>
2022-12-21 23:07:13 -0300

    agregando productos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './component/Navbar';
 import Home from './component/Home';
 
+
+
 function App() {
   return (
     <>
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import Products from './products'
 
 const Home = () => {
     return (
         <div className="hero">
-            <div class="card bg-dark text-white">
-                <img src="/assets/b.jpeg" class="card-img" alt="..."/>
-                    <div class="card-img-overlay">
-                        <h5 class="card-title">Card title</h5>
+            <div class="card bg-dark text-white border-0">
+                <img src="/assets/b.jpeg" class="card-img" alt="fondo"/>
+                    <div class="card-img-overlay d-flex flex-column  justify-content-center">
+                        <div className="container">
+
                         
+                            <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON</h5>
+                            <p className='card-text lead fs-2'>CHECK OUT ALL THE THENDS</p>
+                        </div>
                     </div>
             </div>
+            <Products/>
         </div>
     )
 }
